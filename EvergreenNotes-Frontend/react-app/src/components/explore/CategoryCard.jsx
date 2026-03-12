dae482ec7125c1ec064bca0f9ebe0ecaf3cf4092
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2026-03-12 21:41:09 +0200

    trebding category card

--- EvergreenNotes-Frontend/react-app/src/components/explore/CategoryCard.jsx
+++ EvergreenNotes-Frontend/react-app/src/components/explore/CategoryCard.jsx
@@ -1,9 +1,23 @@
 import "/src/styles/components/explore/category-card.css"
+import topic1 from "../../assets/images/popular1.png"
+import topic2 from "../../assets/images/popular2.png"
+import topic3 from "../../assets/images/popular3.png"
 
 function CategoryCard() {
   return (
     <div className="category-card">
-      <p>Category Card</p>
+      <div className="category-card__content">
+        <img src={topic1} alt="Topic 1" className="category-card__image" />
+        <p>Nature</p>
+      </div>
+      <div className="category-card__content">
+        <img src={topic2} alt="Topic 2" className="category-card__image" />
+        <p>Philosophy</p>
+      </div>
+      <div className="category-card__content">
+        <img src={topic3} alt="Topic 3" className="category-card__image" />
+        <p>Art</p>
+      </div>
     </div>
   )
 }
--- EvergreenNotes-Frontend/react-app/src/components/explore/ExploreSection.jsx
+++ EvergreenNotes-Frontend/react-app/src/components/explore/ExploreSection.jsx
@@ -1,14 +1,33 @@
 import CategoryCard from "./CategoryCard"
 import GardenCard from "./GardenCard"
 import Pagination from "./Pagination"
-import "/src/styles/components/explore/category-card.css"
+import "/src/styles/components/explore/explore-section.css"
 
 function ExploreSection() {
   return (
     <div className="explore-section">
-        <CategoryCard />
-        <GardenCard />
-        <Pagination />
+      <div className="explore-section__header">
+        <h2 className="explore-section__title">Explore</h2>
+        <div className="explore-section__tabs">
+          <div>Trending</div>
+          <div>New</div>
+          <div>Following</div>
+        </div>
+      </div>
+        <div className="explore-section__content-block">
+          <div className="explore-section__content">
+            <h3 className="explore-section__subtitle">Recomended Topics</h3>
+            <CategoryCard />
+          </div>
+          <div className="explore-section__content">
+            <h3 className="explore-section__subtitle">Discover something new</h3>
+            <GardenCard />
+          </div>
+          <div className="explore-section__scrollbar">
+            <div className="explore-section__scrollbar-thumb"></div>
+          </div>
+        </div>
+          <Pagination />
     </div>
   )
 }
