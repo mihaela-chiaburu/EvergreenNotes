65a0dc754759ac8f6a1a3dbd1845f8b418898034
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2026-03-13 13:08:28 +0200

    pagination mockup

--- EvergreenNotes-Frontend/react-app/src/components/explore/Pagination.jsx
+++ EvergreenNotes-Frontend/react-app/src/components/explore/Pagination.jsx
@@ -3,7 +3,11 @@ import "/src/styles/components/explore/pagination.css"
 function Pagination() {
   return (
     <div className="pagination">
-      <p>Pagination</p>
+      <p className="selected">1</p>
+      <p>2</p>
+      <p>3</p>
+      <p className="points">...</p>
+      <p>10</p>
     </div>
   )
 }
